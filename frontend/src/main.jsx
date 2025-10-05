5a87f29794cbc8e83817cca28a5d78d611de9f26
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-10-05 15:22:28 +0530

    Initialize Zerodha Clone project with frontend and backend structure

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <div>
+    <h1>
+      Hello AK
+    </h1>
+   </div>
+  </StrictMode>,
+)
